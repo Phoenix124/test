6821a15799ab6bf01b30aac46b9c9c35eba189b1
maksim.lyahovets <phoenix124@mail.ru>
2018-03-23 09:45:53 +0300

    Change test

--- src/test/java/n4/BiggestNumberFindTest.java
+++ src/test/java/n4/BiggestNumberFindTest.java
@@ -6,8 +6,8 @@ import static org.junit.Assert.assertEquals;
 
 public class BiggestNumberFindTest {
 
-    int[] numbers = {2, 4, 5, 10, 20, 30, 40, 5, 3, 34, 53, 100, 34, 625};
-    Main biggestNumberFinder = new Main();
+    private int[] numbers = {2, 4, 5, 10, 20, 30, 40, 5, 3, 34, 53, 100, 34, 625};
+    private BiggestNumberFinder biggestNumberFinder = new Main() ;
 
     @Test(expected = Error.class)
     public void getBiggestNumber() {
